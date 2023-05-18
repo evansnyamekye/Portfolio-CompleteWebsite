863ae281f05b304b4905c87af27d645e7bf4b1fb
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-05-18 05:03:42 +0000

    Add pop-up-menu

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const navOpen = document.querySelector('#nav-open');
 const closeMenu = document.querySelector('#close-menu');
 const mobileNav = document.querySelector('#mobile-nav');
 const navLinkClose = document.querySelectorAll('.nav-link-close');
-const portfolioItems = document.querySelectorAll('.porfolio-items-detail'); 
-const wrapper = document.querySelector('.wrapper'); 
+const portfolioItems = document.querySelectorAll('.porfolio-items-detail');
+const wrapper = document.querySelector('.wrapper');
 
 navOpen.addEventListener('click', () => {
   mobileNav.style.display = 'flex';
@@ -27,68 +27,70 @@ window.onresize = () => {
   }
 };
 
-const porfolioItemsDetail = [{
-  id: '1',
-  name: 'Tonic',
-  year: '2015',
-  text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has, a galley of type and scrambled it 1960s. Lorem Ipsum is simplydummy text of the printing and typesetting industry.',  
-  title: 'CANOPY',
-  stack: 'Back End Dev',
-  workStack: ['HTML','CSS','JavaScript'],
-  images: 'images/desktop-switch/(1)Snapshoot desktop.png',
-  source: '#',
-  live: '#'
-},
+const porfolioItemsDetail = [
+  {
+    id: '1',
+    name: 'Tonic',
+    year: '2015',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has, a galley of type and scrambled it 1960s. Lorem Ipsum is simplydummy text of the printing and typesetting industry.',
+    title: 'CANOPY',
+    stack: 'Back End Dev',
+    workStack: ['HTML', 'CSS', 'JavaScript'],
+    images: 'images/desktop-switch/(1)Snapshoot desktop.png',
+    source: '#',
+    live: '#',
+  },
 
-{
-  id: '2',
-  name: 'Multi-Post Stories',
-  year: '2015',
-  text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has, a galley of type and scrambled it 1960s. Lorem Ipsum is simplydummy text of the printing and typesetting industry.',  
-  title: 'FACEBOOK',
-  stack: 'Full Stack Dev',
-  workStack: ['HTML', 'Ruby on Rails', 'CSS', 'Javascript'],
-  images: 'images/desktop-switch/(2)Snapshoot desktop.png',
-  source: '#',
-  live: '#'
-},
+  {
+    id: '2',
+    name: 'Multi-Post Stories',
+    year: '2015',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has, a galley of type and scrambled it 1960s. Lorem Ipsum is simplydummy text of the printing and typesetting industry.',
+    title: 'FACEBOOK',
+    stack: 'Full Stack Dev',
+    workStack: ['HTML', 'Ruby on Rails', 'CSS', 'Javascript'],
+    images: 'images/desktop-switch/(2)Snapshoot desktop.png',
+    source: '#',
+    live: '#',
+  },
 
-{
-  id: '3',
-  name: 'Facebook 360',
-  year: '2015',
-  text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has, a galley of type and scrambled it 1960s. Lorem Ipsum is simplydummy text of the printing and typesetting industry.', 
-  title: 'Facebook',
-  stack: 'Full Stack Dev',
-  workStack: ['HTML', 'Ruby on Rails', 'CSS', 'Javascript'],
-  images: 'images/desktop-switch/(3)Snapshoot desktop.png',
-  source: '#',
-  live: '#'
-},
+  {
+    id: '3',
+    name: 'Facebook 360',
+    year: '2015',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has, a galley of type and scrambled it 1960s. Lorem Ipsum is simplydummy text of the printing and typesetting industry.',
+    title: 'Facebook',
+    stack: 'Full Stack Dev',
+    workStack: ['HTML', 'Ruby on Rails', 'CSS', 'Javascript'],
+    images: 'images/desktop-switch/(3)Snapshoot desktop.png',
+    source: '#',
+    live: '#',
+  },
 
-{
-  id: '4',
-  name: 'Uber Navigation',
-  year: '2015',
-  text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has, a galley of type and scrambled it 1960s. Lorem Ipsum is simplydummy text of the printing and typesetting industry.',  
-  title: 'Uber',
-  stack: 'Lead Developer',
-  workStack: ['HTML', 'Ruby on Rails', 'CSS', 'Javascript'],
-  images: 'images/desktop-switch/(4)Snapshoot desktop.png',
-  source: '#',
-  live: '#'
-}];
+  {
+    id: '4',
+    name: 'Uber Navigation',
+    year: '2015',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has, a galley of type and scrambled it 1960s. Lorem Ipsum is simplydummy text of the printing and typesetting industry.',
+    title: 'Uber',
+    stack: 'Lead Developer',
+    workStack: ['HTML', 'Ruby on Rails', 'CSS', 'Javascript'],
+    images: 'images/desktop-switch/(4)Snapshoot desktop.png',
+    source: '#',
+    live: '#',
+  },
+];
 
-if(portfolioItems) {
+if (portfolioItems) {
   portfolioItems.forEach((cards) => {
     cards.addEventListener('click', (e) => {
       const idValue = e.target.closest('.card').id;
-      for (let i =0; i < porfolioItemsDetail.length; i += 1) {
+      for (let i = 0; i < porfolioItemsDetail.length; i += 1) {
         if (porfolioItemsDetail[i].id === idValue) {
           const popupPage = document.createElement('page');
-          popupPage.classList.add("popup-page");
+          popupPage.classList.add('popup-page');
           // console.log(porfolioItemsDetail[i].name);
-          popupPage.innerHTML =  `
+          popupPage.innerHTML = `
           <div class="background"></div>
           <div class="card flex-main">
           <button class="close-btn" type="submit">
@@ -125,13 +127,12 @@ if(portfolioItems) {
           </div>
           `;
           wrapper.appendChild(popupPage);
-          const closeButton = popupPage.querySelector('.close-btn'); 
+          const closeButton = popupPage.querySelector('.close-btn');
           closeButton.addEventListener('click', () => {
-          wrapper.removeChild(popupPage)
+            wrapper.removeChild(popupPage);
           });
         }
       }
-      
     });
   });
 }
